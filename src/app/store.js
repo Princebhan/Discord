e4f435af8dbbd899ecee226ce3559904b9bd7c93
Prince22 <112156035+Princebhan@users.noreply.github.com>
2023-12-19 18:32:10 +0530

    Delete src directory

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import channelReducer from "../features/channelSlice";
-
-export const store = configureStore({
-  reducer: {
-    channel: channelReducer,
-  },
-});
